ca02a600961814491fbb9946b3c088759de5c55f
pengpeng.chen <pengpeng.chen@tongdun.net>
2025-04-08 20:03:43 +0800

    feat: add graph-mind-nebula module to operate nebula graph

---
+++ graph-mind-nebula/graph-nebula-spring-boot-starter/src/main/java/com/chenpp/graph/nebula/config/NebulaAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.chenpp.graph.nebula.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+
+/**
+ * @author April.Chen
+ * @date 2025/4/7 17:00
+ */
+@AutoConfiguration
+public class NebulaAutoConfiguration {
+}
